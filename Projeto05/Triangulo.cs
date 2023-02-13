1d056e3381070ffc8556099aa1f78d16d7fd3a31
Yan Fonseca <yangoular@gmail.com>
2023-02-13 00:33:02 -0300

    Criação dos métodos Perimetro e Diagonal

--- Projeto05/Triangulo.cs
+++ Projeto05/Triangulo.cs
@@ -13,7 +13,11 @@ namespace Projeto05
         public double Area(){
             return Largura * Altura;
         }
-        
-        
+        public double Perimetro(){
+            return 2 * (Largura + Altura);
+        }
+        public double Diagonal(){
+            return Math.Sqrt(Largura * Largura + Altura * Altura);
+        }
     }
 }
