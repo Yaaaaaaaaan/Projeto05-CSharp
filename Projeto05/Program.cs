e312fe8d7bd0c2f2a3b709466bfcc82f7bde0102
Yan Fonseca <yangoular@gmail.com>
2023-02-13 00:45:52 -0300

    Criação de entrada e saída de dados, remoção de bibliotecas não usadas para reduzir o tempo de execução da aplicação.

--- Projeto05/Program.cs
+++ Projeto05/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 
 namespace Projeto05
 {
@@ -10,7 +7,15 @@ namespace Projeto05
     {
         static void Main(string[] args)
         {
+            Triangulo tri = new Triangulo();
+            Console.WriteLine("Insira a altura do Triângulo; ");
+            tri.Altura = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Insira a largura do Triângulo; ");
+            tri.Largura = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
+            Console.WriteLine("Área; " + tri.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perímetro; " + tri.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal; " + tri.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
